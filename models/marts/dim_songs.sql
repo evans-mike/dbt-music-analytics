c2975ff7ece357338f135d8f8f5d28853ab74d17
Mike Evans <mike.evans@fastmail.com>
2024-09-27 18:43:28 +0000

    format

--- models/marts/dim_songs.sql
+++ models/marts/dim_songs.sql
@@ -1,6 +1,1 @@
-
-
-with songs as (select * from {{ ref("stg__songs") }})
-
-select *
-from songs
+with songs as (select * from {{ ref("stg__songs") }}) select * from songs
--- models/staging/stg__song_occurrences.sql
+++ models/staging/stg__song_occurrences.sql
@@ -1,2 +1,1 @@
-select date, title, closer as closer_flag
-from {{ source("raw_data", "song_occurrences") }}
+select date, title, closer_flag from {{ source("raw_data", "song_occurrences") }}
