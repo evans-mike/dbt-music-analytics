bdd654dcc129b0169e34dbf7141f0229c6090256
Mike Evans <mike.evans@fastmail.com>
2024-09-27 13:32:21 +0000

    added tests

--- models/marts/fact_song_occurrences.sql
+++ models/marts/fact_song_occurrences.sql
@@ -41,7 +41,6 @@ with
             title,
             coalesce(closer_flag,false) as closer_flag
         from song_occurrences
-        inner join {{ref("stg__songs")}} using (title)
     )
 
 select *
