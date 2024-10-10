68fc54f297813bbf818c39a3990a35356b3a1e1c
Mike Evans <mike.evans@fastmail.com>
2024-10-10 14:51:58 +0000

    added month_occurred and month_introduced to dim_songs

--- models/marts/dim_songs.sql
+++ models/marts/dim_songs.sql
@@ -17,7 +17,8 @@ select
     songs.*,
     song_introd.introduced,
     format_date('%Y-%m', song_introd.introduced) as month_introduced,
-    song_last_occurred.last_occurred
+    song_last_occurred.last_occurred,
+    format_date('%Y-%m', song_last_occurred.last_occurred) as month_occurred
 from songs
 left join song_introd using (title)
 left join song_last_occurred using (title)
