2be41915ec559b4d2d6d4045141568ca5ea47d69
Mike Evans <15256617+evans-mike@users.noreply.github.com>
2024-10-10 10:52:34 -0400

    New branch (#2)
    * working stg
    
    * next
    
    * formatted fact dates
    
    * added inner join for songs
    
    * added tests
    
    * added source tests
    
    * added source and tests
    
    * updated coalesce
    
    * updated to views
    
    * format
    
    * added introduced column
    
    * added last_occurred
    
    * fixed spelling
    
    * update
    
    * added docs
    
    * resolved merges
    
    * added month_introduced
    
    * added month_occurred and month_introduced to dim_songs

--- models/marts/dim_songs.sql
+++ models/marts/dim_songs.sql
@@ -13,7 +13,12 @@ with
         group by title
     )
 
-select songs.*, song_introd.introduced, song_last_occurred.last_occurred
+select
+    songs.*,
+    song_introd.introduced,
+    format_date('%Y-%m', song_introd.introduced) as month_introduced,
+    song_last_occurred.last_occurred,
+    format_date('%Y-%m', song_last_occurred.last_occurred) as month_occurred
 from songs
 left join song_introd using (title)
 left join song_last_occurred using (title)
