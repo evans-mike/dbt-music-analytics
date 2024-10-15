cb6decd92d1468fae698d00c847a4e60c5a3232a
Mike Evans <mike.evans@fastmail.com>
2024-10-15 12:46:29 +0000

    update occ rate

--- analyses/occurrence_rate.sql
+++ analyses/occurrence_rate.sql
@@ -1,5 +1,5 @@
 select
-    (count(date)/count(distinct title)) occurrence_rate,
+    round((count(date) / count(distinct title)), 2) occurrence_rate,
     {{ get_period("date", "month", 12, 24) }} as period,
 from {{ ref("fact_song_occurrences") }}
 group by period
