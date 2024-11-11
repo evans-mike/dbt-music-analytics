aae7544c6e5f5f08dd04fcbfd9f3d84be7893250
Mike Evans <mike.evans@fastmail.com>
2024-11-11 14:12:22 +0000

    added last_occurred_as_closer

--- models/marts/fact_song_occurrences.sql
+++ models/marts/fact_song_occurrences.sql
@@ -13,10 +13,29 @@ with
         group by title
     ),
 
+    song_last_occurred_as_closer as (
+        select title, max(date) as last_occurred_as_closer
+        from {{ ref("stg__song_occurrences") }}
+        where closer_flag = true
+        group by title
+    ),
+
     christmas_songs as (
         select title, is_christmas
         from {{ ref("dim_songs") }}
         where is_christmas = true
+    ),
+
+    hymn_songs as (
+        select title, is_hymn
+        from {{ ref("dim_songs") }}
+        where is_hymn = true
+    ),
+
+    refrain_songs as (
+        select title, has_refrain
+        from {{ ref("dim_songs") }}
+        where has_refrain = true
     ),
 
     fact_song_occurrences as (
@@ -30,11 +49,17 @@ with
             {{ get_period("song_introd.introduced", "month", 12, 4) }}
             as introduced_period,
             song_last_occurred.last_occurred,
-            christmas_songs.is_christmas
+            song_last_occurred_as_closer.last_occurred_as_closer,
+            christmas_songs.is_christmas,
+            hymn_songs.is_hymn,
+            refrain_songs.has_refrain
         from song_occurrences
         left join christmas_songs using (title)
         left join song_introd using (title)
         left join song_last_occurred using (title)
+        left join song_last_occurred_as_closer using (title)
+        left join hymn_songs using (title)
+        left join refrain_songs using (title)
     )
 
 select *
