7da59fa96aae9d39dcff2ff9925064563759bb34
Mike Evans <mike.evans@fastmail.com>
2024-12-09 15:17:45 +0000

    moved transformations back a level for simplification

--- models/staging/stg__song_occurrences.sql
+++ models/staging/stg__song_occurrences.sql
@@ -1,1 +1,8 @@
-select date, title, closer_flag, service from {{ source("raw_data", "song_occurrences") }}
+select
+    date,
+    title,
+    closer_flag,
+    service,
+    extract(year from date) as year,
+    {{ get_period("date", "month", 12, 4) }} as period
+from {{ source("raw_data", "song_occurrences") }}
