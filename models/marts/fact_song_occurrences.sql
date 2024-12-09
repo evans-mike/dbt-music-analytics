d0b70ddbba2b0db38e23065057c7516b6bdf93b4
Mike Evans <15256617+evans-mike@users.noreply.github.com>
2024-12-09 10:18:06 -0500

    New branch (#13)
    * working stg
    
    * next
    
    * formatted fact dates
    
    * added inner join for songs
    
    * added tests
    
    * added source tests
    
    * added source and tests
    
    * updated coalesce
    
    * updated to views
    
    * format
    
    * added introduced column
    
    * added last_occurred
    
    * fixed spelling
    
    * update
    
    * added docs
    
    * resolved merges
    
    * added month_introduced
    
    * added month_occurred and month_introduced to dim_songs
    
    * removed month_occurred, added year_introduced
    
    * added get_period_year macro
    
    * WIP
    
    * WIP
    
    * resolved str error
    
    * WIP working with macro get_period()
    
    * working stering formatting
    
    * working on occurrence rates
    
    * fix for occurrence rate
    
    * fix intro rate
    
    * update random
    
    * update occ rate
    
    * fixed upcoming dates
    
    * created new agg tables
    
    * added attributes and christmas songs
    
    * updating freshness test
    
    * added hymn and refrain tags
    
    * added last_occurred_as_closer
    
    * added author_group and authors
    
    * added service to occurences
    
    * fix test
    
    * moved transformations back a level for simplification

--- models/marts/fact_song_occurrences.sql
+++ models/marts/fact_song_occurrences.sql
@@ -1,63 +1,36 @@
 with
     song_occurrences as (select * from {{ ref("stg__song_occurrences") }}),
 
-    song_introd as (
-        select title, min(date) as introduced
-        from {{ ref("stg__song_occurrences") }}
-        group by title
-    ),
-
-    song_last_occurred as (
-        select title, max(date) as last_occurred
-        from {{ ref("stg__song_occurrences") }}
-        group by title
-    ),
-
-    song_last_occurred_as_closer as (
-        select title, max(date) as last_occurred_as_closer
-        from {{ ref("stg__song_occurrences") }}
-        where closer_flag = true
-        group by title
-    ),
-
     christmas_songs as (
-        select title, is_christmas from {{ ref("dim_songs") }} where is_christmas = true
+        select title, is_christmas
+        from {{ ref("stg__songs") }}
+        where is_christmas = true
     ),
 
     hymn_songs as (
-        select title, is_hymn from {{ ref("dim_songs") }} where is_hymn = true
+        select title, is_hymn from {{ ref("stg__songs") }} where is_hymn = true
     ),
 
     refrain_songs as (
-        select title, has_refrain from {{ ref("dim_songs") }} where has_refrain = true
+        select title, has_refrain from {{ ref("stg__songs") }} where has_refrain = true
     ),
 
     fact_song_occurrences as (
         select
-            date,
-            extract(year from date) as year,
-            {{ get_period("date", "month", 12, 4) }} as period,
-            title,
-            dim_songs.author_group,
-            dim_songs.authors,
-            closer_flag,
-            service,
-            song_introd.introduced,
-            {{ get_period("song_introd.introduced", "month", 12, 4) }}
-            as introduced_period,
-            song_last_occurred.last_occurred,
-            song_last_occurred_as_closer.last_occurred_as_closer,
+            song_occurrences.*,
+            stg__songs.author_group,
+            stg__songs.introduced,
+            stg__songs.introduced_period,
+            stg__songs.last_occurred,
+            stg__songs.last_occurred_as_closer,
             christmas_songs.is_christmas,
             hymn_songs.is_hymn,
             refrain_songs.has_refrain
         from song_occurrences
         left join christmas_songs using (title)
-        left join song_introd using (title)
-        left join song_last_occurred using (title)
-        left join song_last_occurred_as_closer using (title)
         left join hymn_songs using (title)
         left join refrain_songs using (title)
-        left join {{ ref("dim_songs") }} as dim_songs using (title)
+        left join {{ ref("stg__songs") }} using (title)
     )
 
 select *
--- models/staging/stg__songs.sql
+++ models/staging/stg__songs.sql
@@ -1,1 +1,38 @@
-select title, attributes, author_group, authors from {{ source("raw_data", "songs") }}
+with
+    song_introd as (
+        select title, min(date) as introduced
+        from {{ source("raw_data", "song_occurrences") }}
+        group by title
+    ),
+
+    song_last_occurred as (
+        select title, max(date) as last_occurred
+        from {{ source("raw_data", "song_occurrences") }}
+        group by title
+    ),
+
+    song_last_occurred_as_closer as (
+        select title, max(date) as last_occurred_as_closer
+        from {{ source("raw_data", "song_occurrences") }}
+        where closer_flag = true
+        group by title
+    )
+
+select
+    title,
+    attributes,
+    author_group,
+    authors,
+    case
+        when songs.attributes like '%christmas%' then true else false
+    end as is_christmas,
+    case when songs.attributes like '%hymn%' then true else false end as is_hymn,
+    case when songs.attributes like '%refrain%' then true else false end as has_refrain,
+    song_introd.introduced,
+    {{ get_period("song_introd.introduced", "month", 12, 4) }} as introduced_period,
+    song_last_occurred.last_occurred,
+    song_last_occurred_as_closer.last_occurred_as_closer
+from {{ source("raw_data", "songs") }}
+left join song_introd using (title)
+left join song_last_occurred using (title)
+left join song_last_occurred_as_closer using (title)
