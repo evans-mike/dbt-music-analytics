34fc4a68f0b963a984ce7869b329d5281b099fea
Mike Evans <mike.evans+rcbc@fastmail.com>
2025-03-28 17:04:07 +0000

    added dense_rank() to apply a rank column

--- models/reports/agg_closers.sql
+++ models/reports/agg_closers.sql
@@ -1,16 +1,19 @@
-select
-    *,
-    coalesce(`000-052weeks`, 0)
-    + coalesce(`052-104weeks`, 0)
-    + coalesce(`104-156weeks`, 0)
-    + coalesce(`156-208weeks`, 0) as grand_total
+select dense_rank() over (order by `000-052weeks` desc) as rank, *
 from
     (
-        select title, last_occurred_as_closer, date, period
-        from {{ ref("fact_song_occurrences") }}
-        where closer_flag = true
-    ) pivot (
-        count(date) for period
-        in ('000-052weeks', '052-104weeks', '104-156weeks', '156-208weeks')
+        select *
+        -- ,coalesce(`000-052weeks`, 0)
+        -- + coalesce(`052-104weeks`, 0)
+        -- + coalesce(`104-156weeks`, 0)
+        -- + coalesce(`156-208weeks`, 0) as grand_total
+        from
+            (
+                select title, last_occurred_as_closer, date, period
+                from {{ ref("fact_song_occurrences") }}
+                where closer_flag = true
+            ) pivot (
+                count(date) for period
+                in ('000-052weeks', '052-104weeks', '104-156weeks', '156-208weeks')
+            )
     )
 order by `000-052weeks` desc
