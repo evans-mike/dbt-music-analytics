37f1aea57fc4e011fb804131692b7eff1ce56d76
Mike Evans <15256617+evans-mike@users.noreply.github.com>
2025-04-28 16:35:21 -0400

    New branch (#24)
    * working stg
    
    * next
    
    * formatted fact dates
    
    * added inner join for songs
    
    * added tests
    
    * added source tests
    
    * added source and tests
    
    * updated coalesce
    
    * updated to views
    
    * format
    
    * added introduced column
    
    * added last_occurred
    
    * fixed spelling
    
    * update
    
    * added docs
    
    * resolved merges
    
    * added month_introduced
    
    * added month_occurred and month_introduced to dim_songs
    
    * removed month_occurred, added year_introduced
    
    * added get_period_year macro
    
    * WIP
    
    * WIP
    
    * resolved str error
    
    * WIP working with macro get_period()
    
    * working stering formatting
    
    * working on occurrence rates
    
    * fix for occurrence rate
    
    * fix intro rate
    
    * update random
    
    * update occ rate
    
    * fixed upcoming dates
    
    * created new agg tables
    
    * added attributes and christmas songs
    
    * updating freshness test
    
    * added hymn and refrain tags
    
    * added last_occurred_as_closer
    
    * added author_group and authors
    
    * added service to occurences
    
    * fix test
    
    * moved transformations back a level for simplification
    
    * added year
    
    * updated get_period references to weeks
    
    * added new pivot report
    
    * added rolling annual aggs
    
    * created reports folder
    
    * finished remaining reports
    
    * added introduced
    
    * updated ordering
    
    * added dense_rank() to apply a rank column
    
    * added rank to all_time
    
    * added order by last_occurred
    
    * added familiarity score
    
    * fixed familiarity
    
    * updated familiarity
    
    ---------
    
    Co-authored-by: Mike Evans <mike.evans+rcbc@fastmail.com>

--- macros/familiarity_score.sql
+++ macros/familiarity_score.sql
@@ -1,15 +1,14 @@
-{% macro familiarity_score(occurrences, weeks_window, freshness_score) %}
+{% macro familiarity_score(occurrences, freshness_score) %}
     round(
         case
             when {{ occurrences }} is null
             then 0
-            when {{ weeks_window }} is null
-            then 0
             when {{ freshness_score }} is null
             then 0
             else
-                least({{ occurrences }} / ((8 / 52.0) * {{ weeks_window }}), 1.0)
+                least({{ occurrences }} / 7.0, 1.0)
                 * ({{ freshness_score }} / 100)
+                * least({{ occurrences }} / 4.0, 1.0)
                 * 100
         end,
         2
--- models/reports/agg_occurrences_per_year.sql
+++
@@ -1,4 +0,0 @@
-select period, count(1) count_occurrences
-from {{ ref("fact_song_occurrences") }}
-group by period
-order by period
