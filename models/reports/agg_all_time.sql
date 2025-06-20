49d9c5a8066ac91edaa25c38a3a6fee702f73fdf
Mike Evans <mike.evans+rcbc@fastmail.com>
2025-06-20 18:46:29 +0000

    simplified all time

--- models/reports/agg_all_time.sql
+++ models/reports/agg_all_time.sql
@@ -3,22 +3,36 @@ from
     (
         select
             *,
-            coalesce(`000-052weeks`, 0)
-            + coalesce(`052-104weeks`, 0)
-            + coalesce(`104-156weeks`, 0)
-            + coalesce(`156-208weeks`, 0) as grand_total
+            coalesce(`Within 1 Year`, 0)
+            + coalesce(`1-2 Years Ago`, 0)
+            + coalesce(`2-3 Years Ago`, 0)
+            + coalesce(`3-4 Years Ago`, 0) as grand_total
         from
             (
                 select
-                    title,
-                    is_active,
-                    last_occurred,
-                    date,
-                    {{ get_period("date", "week", 52, 8) }} as period
-                from {{ ref("fact_song_occurrences") }}
+                    * except (period),
+                    case
+                        when period = '000-052weeks'
+                        then 'Within 1 Year'
+                        when period = '052-104weeks'
+                        then '1-2 Years Ago'
+                        when period = '104-156weeks'
+                        then '2-3 Years Ago'
+                        when period = '156-208weeks'
+                        then '3-4 Years Ago'
+                    end as period
+                from
+                    (
+                        select
+                            title,
+                            last_occurred,
+                            date,
+                            {{ get_period("date", "week", 52, 8) }} as period
+                        from {{ ref("fact_song_occurrences") }}
+                    )
             ) pivot (
                 count(date) for period
-                in ('000-052weeks', '052-104weeks', '104-156weeks', '156-208weeks')
+                in ('Within 1 Year', '1-2 Years Ago', '2-3 Years Ago', '3-4 Years Ago')
             )
     )
 order by grand_total desc, last_occurred desc
