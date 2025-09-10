2dbbb6c885ea0a1fdfbb97d7d7e08c924c2da26c
Mike Evans <mike.evans+rcbc@fastmail.com>
2025-09-10 19:56:06 +0000

    refined retention period

--- models/reports/agg_retention_rate.sql
+++ models/reports/agg_retention_rate.sql
@@ -1,4 +1,3 @@
-/* Compare individual song retention, period over period. e.g. What was retention rate from 4 periods ago to 3 years ago? Then to 2 years ago, and then to current year? */
 with
     songs_by_period as (
         select {{ get_period("date", "week", 52, 8) }} as period, title
@@ -10,33 +9,26 @@ with
         from (select distinct period from songs_by_period)
     ),
     period_chain as (
-        select period, lead(period) over (order by period_int) as next_period
+        select period, lead(period) over (order by period_int) as last_period
         from period_map
     ),
     songs_retained as (
         select
             e1.period,
             e1.title,
-            case when e2.title is not null then 1 else 0 end as retained_next_period
+            case when e2.title is not null then 1 else 0 end as retained_last_period
         from songs_by_period e1
         left join period_chain pc on pc.period = e1.period
         left join
-            songs_by_period e2 on e2.title = e1.title and e2.period = pc.next_period
+            songs_by_period e2 on e2.title = e1.title and e2.period = pc.last_period
     )
 
--- select * from songs_by_period;
--- select * from period_map;
--- select * from period_chain;
--- select * from songs_retained;
 select
     f.period,
-    count(*) as songs_considered,
-    sum(retained_next_period) as retained_count,
-    round(100.0 * avg(retained_next_period), 2) as retention_rate_pct
+    count(*) as current_title_count,
+    sum(retained_last_period) as retained_title_count,
+    round(100.0 * avg(retained_last_period), 2) as retention_rate_pct
 from songs_retained f
 join period_map pm on pm.period = f.period
 group by period
-order by
-    period
-    -- ;
-    
+order by period desc
