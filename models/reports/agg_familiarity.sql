7cccc7269172b81c15793e9f10da017c798195a0
Mike Evans <15256617+evans-mike@users.noreply.github.com>
2025-12-19 13:50:33 -0500

    New branch (#51)
    * working stg
    
    * next
    
    * formatted fact dates
    
    * added inner join for songs
    
    * added tests
    
    * added source tests
    
    * added source and tests
    
    * updated coalesce
    
    * updated to views
    
    * format
    
    * added introduced column
    
    * added last_occurred
    
    * fixed spelling
    
    * update
    
    * added docs
    
    * resolved merges
    
    * added month_introduced
    
    * added month_occurred and month_introduced to dim_songs
    
    * removed month_occurred, added year_introduced
    
    * added get_period_year macro
    
    * WIP
    
    * WIP
    
    * resolved str error
    
    * WIP working with macro get_period()
    
    * working stering formatting
    
    * working on occurrence rates
    
    * fix for occurrence rate
    
    * fix intro rate
    
    * update random
    
    * update occ rate
    
    * fixed upcoming dates
    
    * created new agg tables
    
    * added attributes and christmas songs
    
    * updating freshness test
    
    * added hymn and refrain tags
    
    * added last_occurred_as_closer
    
    * added author_group and authors
    
    * added service to occurences
    
    * fix test
    
    * moved transformations back a level for simplification
    
    * added year
    
    * updated get_period references to weeks
    
    * added new pivot report
    
    * added rolling annual aggs
    
    * created reports folder
    
    * finished remaining reports
    
    * added introduced
    
    * updated ordering
    
    * added dense_rank() to apply a rank column
    
    * added rank to all_time
    
    * added order by last_occurred
    
    * added familiarity score
    
    * fixed familiarity
    
    * updated familiarity
    
    * added is_retired flag for songs
    
    * added emoji booleans
    
    * updated is_retired to is_active
    
    * ordered by familiarity_score
    
    * added familiarity_score
    
    * added is_corporate flag on songs
    
    * added new report agg_corporate
    
    * simplified agg all time
    
    * moved get_period() lower and added 6 month agg
    
    * removed period from select list
    
    * fix
    
    * fix
    
    * removed period
    
    * simplified all time
    
    * modified add_all_time
    
    * added new view for recent drop offs
    
    * updated drop offs
    
    * updated introduced to include author details
    
    * WIP on retention rate
    
    * refined retention period
    
    * refined queries
    
    * WIP intro rate
    
    * working intro rate v2
    
    * merged retention and introduction rates
    
    * updated percentage
    
    * updated scale to months
    
    * added null checks for raw data
    
    * updated average model
    
    * added totals and removed christmas from rentention rate
    
    * added count occurrences
    
    ---------
    
    Co-authored-by: Mike Evans <mike.evans+rcbc@fastmail.com>

--- models/reports/agg_familiarity.sql
+++ models/reports/agg_familiarity.sql
@@ -1,3 +1,9 @@
-select familiarity_score, title, last_occurred,
+select familiarity_score, title, count_occurrences, last_occurred
 from {{ ref("dim_songs") }}
+join
+    (
+        select title, count(date) count_occurrences
+        from {{ ref("fact_song_occurrences") }}
+        group by title
+    ) using (title)
 order by familiarity_score desc, title
